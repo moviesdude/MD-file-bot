7a52ef2007399e8e65f7c617f88a477f3d937979
DevWilson24 <118274743+myselfdev24@users.noreply.github.com>
2023-03-14 19:20:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-#Thank you LazyDeveloper for helping me in this journey !
-#Must Subscribe On YouTube @LazyDeveloperr 
+
 
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '@LazyDeveloper'
+    return '@MD_Bots'
 
 if __name__ == "__main__":
     app.run()
